7d8bed28ec767a8e89b89cbf535e8a49491bc1aa
Zachary Iles <zack.iles@gmail.com>
2015-04-16 17:21:40 -0400

    fx tabbing in test

--- test/index.js
+++ test/index.js
@@ -1,77 +1,81 @@
 var assert = require('assert'),
-    Promise = require('bluebird'),
-    PromiseQueue = require('../index.js');
-    
-var deadPromise = function(args, delay){
-	return new Promise(function(resolve, reject){resolve(args);}).delay(delay||0);
+  Promise = require('bluebird'),
+  PromiseQueue = require('../index.js');
+
+var deadPromise = function(args, delay) {
+  return new Promise(function(resolve, reject) {
+    resolve(args);
+  }).delay(delay || 0);
 };
 
-describe('Methods', function(){
-	
-	beforeEach(function(done) {
-		this.promiseQueue = new PromiseQueue();
-		this.promiseQueue.add(deadPromise, 'hello');
-		done();
-	});
-	
-	describe('#add()', function(){
-		it('should add a promise', function(done){
-			assert.strictEqual(this.promiseQueue._queue.length, 1);
-			done();
-		});
-	});
-	
-	describe('#start()', function(){
-		
-		it('return the resolved promises', function(done){
-			this.promiseQueue.start().then(function(results){
-				assert.strictEqual(results.length, 1);
-				assert.strictEqual(results[0], 'hello');
-				done();
-			}).catch(done);
-		});
-		
-		it('return many resolved promises with concurrency and FIFO', function(done){
-			this.promiseQueue = new PromiseQueue();
-			// index starts with 1 -> javascript tom foolery
-			for (var i = 1; i < 8; i++) {
-				this.promiseQueue.add(deadPromise, i);
-			}
-			assert.strictEqual(this.promiseQueue._queue.length, 7);
-			this.promiseQueue.start().then(function(results){
-				for (var i = 1; i < results.length; i++) {
-					// i - 1 -> javascript tom foolery
-					assert.strictEqual(results[i-1], i);
-				}
-				done();
-			}).catch(done);
-		});
-		
-		it('resolve promise with delay', function(done){
-			this.timeout(12000);
-			this.promiseQueue = new PromiseQueue({delay: 10000});
+describe('Methods', function() {
 
-			var future = new Date();
-			future.setSeconds(future.getSeconds() + 10);
-			
-			this.promiseQueue.start().then(function(results){
-				assert.strictEqual(new Date() > future, true);
-				done();
-			}).catch(done);
-		});
-		
-	});
-	
-	describe('#drain()', function(){
-		it('should drain all promises', function(done){
-			var self = this;
-			this.promiseQueue.onComplete = function(){
-				assert.strictEqual(self.promiseQueue._queue.length, 0);
-				done();
-			};
-			this.promiseQueue.onError = done;
-			this.promiseQueue.drain();
-		});
-	});
-	
+  beforeEach(function(done) {
+    this.promiseQueue = new PromiseQueue();
+    this.promiseQueue.add(deadPromise, 'hello');
+    done();
+  });
+
+  describe('#add()', function() {
+    it('should add a promise', function(done) {
+      assert.strictEqual(this.promiseQueue._queue.length, 1);
+      done();
+    });
+  });
+
+  describe('#start()', function() {
+
+    it('return the resolved promises', function(done) {
+      this.promiseQueue.start().then(function(results) {
+        assert.strictEqual(results.length, 1);
+        assert.strictEqual(results[0], 'hello');
+        done();
+      }).catch(done);
+    });
+
+    it('return many resolved promises with concurrency and FIFO', function(done) {
+      this.promiseQueue = new PromiseQueue();
+      // index starts with 1 -> javascript tom foolery
+      for (var i = 1; i < 8; i++) {
+        this.promiseQueue.add(deadPromise, i);
+      }
+      assert.strictEqual(this.promiseQueue._queue.length, 7);
+      this.promiseQueue.start().then(function(results) {
+        for (var i = 1; i < results.length; i++) {
+          // i - 1 -> javascript tom foolery
+          assert.strictEqual(results[i - 1], i);
+        }
+        done();
+      }).catch(done);
+    });
+
+    it('resolve promise with delay', function(done) {
+      this.timeout(12000);
+      this.promiseQueue = new PromiseQueue({
+        delay: 10000
+      });
+
+      var future = new Date();
+      future.setSeconds(future.getSeconds() + 10);
+
+      this.promiseQueue.start().then(function(results) {
+        assert.strictEqual(new Date() > future, true);
+        done();
+      }).catch(done);
+    });
+
+  });
+
+  describe('#drain()', function() {
+    it('should drain all promises', function(done) {
+      var self = this;
+      this.promiseQueue.onComplete = function() {
+        assert.strictEqual(self.promiseQueue._queue.length, 0);
+        done();
+      };
+      this.promiseQueue.onError = done;
+      this.promiseQueue.drain();
+    });
+  });
+
 });
